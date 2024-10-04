2da8a13977750ad472a9167913bcd67868bfad5f
Gold Lee <USER@GoldLee.GoldLee001>
2024-10-04 16:35:30 +0100

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const menu = document.querySelector('.menu')
+    const close = document.querySelector('.close');
+    const navUl = document.querySelector('.nav');
+
+    menu.addEventListener('click', function() {
+        navUl.classList.add('show');
+        menu.style.display = 'none'
+        close.style.display = 'block'
+    });
+    
+    close.addEventListener('click', function() {
+        navUl.classList.remove('show');
+        close.style.display = 'none'
+        menu.style.display = 'block'
+    });
+});
+
+let randomArray = [
+    'Thank you for the tracks', 'Gotten the logs and they are perfect. Thanks', 'The SMTP wasn\'t all that stable but it\'s worth it', 'First time trying I hope the views stays on my TikTok', 'Please can you send me the full documentation of the SQR400 I purchased',
+    'Thanks the software is great. I love it but why does it need strong internet connection to get more cool operation.', 'Please why did it take long for the CC to stampigate', 'Great track 2 dump', 'I will still come purchase more logs. You guys are Great',
+    'Cool logs and very active', 'Thank you for getting my Facebook account back after all this year\'s if being hacked', 'fast, adequate and accurate account sorting this software really rocks', 'More Asia CCV please I need more. Your CCV are dope #fighting',
+    'Clone my company database please.', 'I paid already and sent my payment proofs please check', 'Great software and easy to use but make provision for using the software offline, sometimes one might be out of data (internet connection)and needs to make ugent transaction which is not possible because of limited internet access',
+    'Your dumps are cracking on my end. Why ?', 'Best dumps ever. Coming back for more', 'Thanks you for this', 'Thanks I got your mail', 'I just made my deposit', 'Thank you I received the followers', 'Now I know how dj Khalid gets more YouTube views', 'My followers are growing yes thank you',
+    'Sorry I posted my review more than once. I am so excited to get this URL', 'Cool software to use it is very reliable , some fake online website that shows different kind videos and then they\'ve given you rubbish log. Thanks so much for the software the charges is token compared to it\'s quality.'
+ ];
+
+ let randomElement = randomArray[Math.floor(Math.random() * randomArray.length)];
+ let randomElementb = randomArray[Math.floor(Math.random() * randomArray.length)];
+ let randomElementc = randomArray[Math.floor(Math.random() * randomArray.length)];
+
+ document.getElementById("feedbacka").innerHTML = randomElement;
+ document.getElementById("feedbackb").innerHTML = randomElementb;
+ document.getElementById("feedbackc").innerHTML = randomElementc;
+
+ function review() {
+    if (!$("#msg").val().trim()) {
+       alert("Please enter your review.");
+       return;
+    }
+    alert("Your review has been successfully submitted.")
+ }
